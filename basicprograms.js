64859eb6566266f86687b766f75c2a54e9799eaa
Praveen Kumar <praveenkumar2k4.t@gmail.com>
2024-12-29 08:49:07 +0530

    javascript basic programs

---
+++ basicprograms.js
@@ -0,0 +1,158 @@
+//1---------------------------//
+
+// var a=5;
+// console.log(a);
+         
+//2---------------------------//
+
+// function fun(){
+//     if(true) {
+//         var a=5;
+//         console.log(a);
+//     };
+//     console.log(a);
+// };
+// fun();
+
+//3---------------------------//
+
+// let a=5;
+// a=3;
+// console.log(a);
+
+//4---------------------------//
+
+// const a=5;
+// console.log(a);
+
+//5---------------------------//
+
+// let a=5;
+// if(a%2==0) {
+//     console.log('Even');
+// } else {
+//     console.log('Odd');
+// }   
+
+//6---------------------------//
+
+// let person = {
+//     name: "pk",
+//     age: 21,
+//     phno: 6374948623
+// };
+// console.log(person.name + " " + person.age + " " + person.phno);
+
+//7---------------------------//
+
+// var colors = ["Red", "Green", "Blue"];
+// console.log(colors);
+
+//8---------------------------//
+
+// let a = {
+//     name : ["pk","sathish","nisanth"],
+//     age : 21
+// };
+// console.log(a.name[0] + " " + a.age);
+
+//9---------------------------//
+
+// let stu = new Object();
+// stu.name="pk";
+// stu.age="20";
+// console.log(stu);
+// console.log(stu.name + " " + stu.age);
+// console.log(stu.name);
+// console.log(stu.age);
+
+//10---------------------------//
+
+// let stu = new Object();
+// stu.name="pk";
+// stu.age="20";
+// stu.hel=function() {
+//     console.log("Praveen MCA");
+// };
+// stu.hel();
+// console.log(stu.name + " " + stu.age);
+
+//11---------------------------//
+
+// let x = 10;
+// let y = 5;
+// console.log("Addition: " + (x + y));
+// console.log("Subtraction: " + (x - y));
+// console.log("Multiplication: " + (x * y));
+// console.log("Division: " + (x / y));
+// console.log("Modulus: " + (x % y));
+
+//12---------------------------//
+
+// let m = 10;
+// let n = 5;
+// console.log("Equal: " + (m == n));
+// console.log("Not Equal: " + (m != n));
+// console.log("Greater than: " + (m > n));
+// console.log("Less than: " + (m < n));
+// console.log("Greater than or equal to: " + (m >= n));
+// console.log("Less than or equal to: " + (m <= n));
+
+//13---------------------------//
+
+// let a = true;
+// let b = false;
+// console.log("Logical AND: " + (a && b));   //sortcircuit operator
+// console.log("Logical OR: " + (a || b));    //sortcircuit operator
+// console.log("Logical NOT: " + (!a));
+
+//14---------------------------//
+
+// let z = 10;
+// z += 5;
+// console.log("z += 5: " + z);
+// z -= 3;
+// console.log("z -= 3: " + z);
+// z *= 2;
+// console.log("z *= 2: " + z);
+// z /= 4;
+// console.log("z /= 4: " + z);
+// z %= 3;
+// console.log("z %= 3: " + z);
+
+//15---------------------------//
+
+// let i = 5;
+// let j = 5;
+// console.log("Post Increment: " + i++);
+// console.log("Pre Increment: " + ++j);
+
+//16---------------------------//
+
+// let a = 5;
+// let b = a++ + --a;
+// let c = ++b + b++;
+// a++;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+//17---------------------------//
+
+// let a=true;
+// !!!!!a;       //that is not declare so that line doesn't consider
+// console.log(a);
+
+//18---------------------------//
+
+// let x = 5;                                          // 0101 in binary
+// let y = 3;                                          // 0011 in binary
+// console.log("Bitwise AND: " + (x & y));             // 0001 in binary, which is 1
+// console.log("Bitwise OR: " + (x | y));              // 0111 in binary, which is 7
+// console.log("Bitwise XOR: " + (x ^ y));             // 0110 in binary, which is 6
+// console.log("Bitwise NOT: " + (~x));                // 1010 in binary (two's complement), which is -6
+// console.log("Left Shift: " + (x << 1));             // 1010 in binary, which is 10
+// console.log("Right Shift: " + (x >> 1));            // 0010 in binary, which is 2
+// console.log("Zero-fill Right Shift: " + (x >>> 1)); // 0010 in binary, which is 2
+
+//---------------------------//
---
+++ day2.js
@@ -0,0 +1,145 @@
+//1---------------------------//
+
+// let a=15;
+// if(a%3==0)
+//     console.log('hi');
+// if(a%5==0){
+//     console.log('hello');
+//}
+
+//2---------------------------//
+
+// choice = 2;
+// switch(choice){
+//     case 1:
+//         console.log('hi');
+//         break;
+//     case 2:
+//         console.log('hello');
+//         break;
+//     default:
+//         console.log('bye');
+//         break;
+// }
+
+//3---------------------------//
+
+// let arr = [1,2,3,4,5];
+// for(let i=0;i<5;i++){
+//     console.log(arr[i]);
+// }
+
+//4---------------------------//
+
+// let arr = [1,2];
+// switch(arr) {
+//     case 1:
+//         console.log('hi');
+//         break;
+//     case 2:
+//         console.log('hello');
+//         break;
+//     default:
+//         console.log('bye');
+//         break;
+// }
+
+//5---------------------------//
+
+// let a=10;
+// for(i=1;i<=a;i++){
+//     if(i%2==0) {
+//         console.log(i);
+//     }
+// }
+
+
+//6---------------------------//
+
+// let arr = [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9]
+// ];
+// for(let i=0;i<3;i++){
+//     for(let j=0;j<3;j++){
+//         console.log(arr[i][j]);
+//     }
+// }
+
+//7---------------------------//
+
+// let a=10;
+// while(a>0){
+//     console.log('hi');
+//     break;
+// }
+
+//8---------------------------//
+
+// let orange = () => "orange";
+// console.log(orange());
+
+//9---------------------------//
+
+// let banana = () => {
+//     console.log('pk');
+//     return 10+8;
+// }
+// console.log(banana());
+
+//10---------------------------//
+
+// function cat(callback){
+//     setTimeout (() => {
+//         console.log('Tom the cat');
+//         callback();
+//     }, 2000);
+// }
+// function mouse(){
+//     console.log('Jerry the mouse');
+// }
+// cat(mouse);
+
+//11---------------------------//
+
+// function attandance(callback){
+//     console.log('Attandance is marked');
+//     callback();
+// }
+// function lunch(callback){
+//     console.log('Lunch time');
+//     callback();
+// }
+// function goingHome(callback){
+//     console.log('Going home');
+//     callback();
+// }
+// function dayComplete(){
+//     console.log('Day is completed');
+// }
+// attandance(() => {
+//     lunch(() => {
+//         goingHome(() => {
+//             dayComplete();
+//         });
+//     });
+// });
+
+//12---------------------------//
+
+//      ___________
+//     |           |
+//  !--|   O   O   |--!
+//     |     ^     |
+//     |   \___/   |
+//     |___________|
+console.log("     ___________");
+console.log("    |           |");
+console.log(" !--|   o   o   |--!");
+console.log("    |     ^     |");
+console.log("    |   \\___/   |");
+console.log("    |___________|");
+
+//13---------------------------//
+
